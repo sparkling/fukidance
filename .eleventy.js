474cc8e77eeee8b9f5e806d2216c2e334731ad56
Henrik Pettersen <henrik@sparklingideas.co.uk>
2020-07-18 22:00:06 +0100

    eleventy initial

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setTemplateFormats([
+    "md",
+    "mp4",
+    "woff",
+    "woff2",
+    "html",
+    "css" // css is not yet a recognized template extension in Eleventy
+  ]);
+  return {
+    dir: {
+      includes: "_includes",
+      layouts: "layouts"
+    }
+  }
+};
---
+++ about.md
@@ -0,0 +1,8 @@
+---
+title: About Me
+description: Description
+date: "2012-04-06"
+layout: layout.njk
+---
+
+# aboutme...
